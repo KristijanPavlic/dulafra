82bd1930ff660ce20ceb8ac1f6e355c8139ea9c1
KristijanPavlic <pavlictumpakristijan@gmail.com>
2024-06-29 11:36:02 +0200

    added more SEO and ico

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -10,16 +10,67 @@ import { ReactNode } from 'react'
 const montserrat = Montserrat({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Foto corner',
+  title:
+    'Foto corner - Fotografiranje sportskih događaja i foto proizvodi po narudžbi',
+  keywords: [
+    'sports photography',
+    'Foto Corner',
+    'Dulafra',
+    'event photography',
+    'custom photo products',
+    'photo mugs',
+    'photo posters',
+    'Fifa cards',
+    'custom Fifa cards',
+    'sports event photos',
+    'photo gallery',
+    'sports photography portfolio',
+    'Dulafra photographer',
+    'Dulafra photo products',
+    'Dulafra portfolio',
+    'sportska fotografija',
+    'Kutak za fotografije',
+    'Dulafra',
+    'fotografija događaja',
+    'prilagođeni foto proizvodi',
+    'foto šalice',
+    'foto posteri',
+    'Fifa kartice',
+    'prilagođene Fifa kartice',
+    'fotografije sportskog događaja',
+    'FOTOGALERIJA',
+    'portfolio sportske fotografije',
+    'Dulafra fotograf',
+    'Dulafra foto proizvodi',
+    'Dulafra portfelj'
+  ],
   description:
     'Fotografiranje sportskih događaja. Izrada fotografija, šalica, postera i Fifa kartica sa slikama igrača po vlastitom izboru',
   authors: { name: 'Kristijan Pavlic Tumpa' },
+  robots: 'index, follow',
   icons: [
     {
       url: './favicon.ico',
-      href: './favicon.ico'
+      href: './favicon.ico',
+      type: 'image/ico'
     }
-  ]
+  ],
+  openGraph: {
+    title:
+      'Foto corner - Fotografiranje sportskih događaja i foto proizvodi po narudžbi',
+    description:
+      'Fotografiranje sportskih događaja. Izrada fotografija, šalica, postera i Fifa kartica sa slikama igrača po vlastitom izboru.',
+    url: 'https://dulafra.com',
+    type: 'website',
+    images: [
+      {
+        url: 'https://dulafra.com/favicon.ico',
+        width: 32,
+        height: 32,
+        alt: 'Foto Corner'
+      }
+    ]
+  }
 }
 
 export default function RootLayout({
